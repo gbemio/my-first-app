4b461f4f22f118e93185cc359616cd97bb3dcb4e
Gbemisola Odukoya <folaryz@gmail.com>
2022-05-25 01:02:50 +0100

    BEM Naming Convention

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,54 @@
 import './App.css';
+import Header from "./Header";
+
+// BEM - Block Element Modifier
 
 function App() {
-  const name = 'sonny'
-const isMale = false
-
-function getGreeting(user) {
-if (user) {
-  return <h1>Hello, {user}!</h1>;
-}
-return <h1>Hello, Stranger.</h1>;
-}
-
   return (
     <div className="app">
-  {getGreeting( )}
-  <h2>You are a {isMale ? "Male" : "Female"}</h2>
+      <Header />
+      <h1 className="app__title">Hello World</h1>
+      <h1 className="app__title app__title--large">This is a large title</h1>
+      <h1 className="app__title app__title--small">This is a small title</h1>
     </div>
-  );
-
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+  )
 }
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
 
-return (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>);
+  // introduction to jsx
 
-}
+  // function App() {
+//   const name = 'sonny'
+// const isMale = false
+
+// function getGreeting(user) {
+// if (user) {
+//   return <h1>Hello, {user}!</h1>;
+// }
+// return <h1>Hello, Stranger.</h1>;
+// }
+
+//   return (
+//     <div className="app">
+//   {getGreeting( )}
+//   <h2>You are a {isMale ? "Male" : "Female"}</h2>
+//     </div>
+//   );
+
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// const user = {
+//   firstName: 'Harper',
+//   lastName: 'Perez'
+// };
+
+// return (
+//   <h1>
+//     Hello, {formatName(user)}!
+//   </h1>);
+
+// }
 
 export default App;
